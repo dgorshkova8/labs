9b01133323f22ef633858d42c6ff2bfad96cc1c4
Darya <d.gorshkova8@gmail.com>
2020-12-16 04:48:40 +0700

    Matrix init

---
+++ Labs/Matrix.cpp
@@ -0,0 +1,124 @@
+#include "Matrix.h"
+// Переменная для создания уникального id
+unsigned int Matrix::idCounter = 0;
+
+Matrix::Matrix(int columnsCount, int rowsCount)
+{
+	id = idCounter;
+	idCounter++;
+
+	this->columnsCount = columnsCount;
+	this->rowsCount = rowsCount;
+
+	// Выделяем память для строк
+	data = new int* [rowsCount];
+	for (int i = 0; i < rowsCount; i++)
+	{
+		// Выделяем память для элементов строк
+		data[i] = new int[columnsCount];
+
+		// Заполняем нулями вновь созданные элементы строк
+		for (int j = 0; j < columnsCount; j++)
+			data[i][j] = 0;
+	}
+}
+
+
+
+Matrix::Matrix() : Matrix::Matrix(3, 3) /* Вызываем конструктор матрицы 3х3*/ {}
+
+Matrix::Matrix(const Matrix& obj)
+{
+	// Как конструктор с параметрами, только данные берём из переданного объкта
+	id = idCounter;
+	idCounter++;
+
+	columnsCount = obj.columnsCount;
+	rowsCount = obj.rowsCount;
+
+	data = new int* [rowsCount];
+	for (int i = 0; i < rowsCount; i++)
+	{
+		data[i] = new int[columnsCount];
+		for (int j = 0; j < columnsCount; j++)
+			data[i][j] = obj.data[i][j];
+	}
+}
+
+Matrix::~Matrix()
+{
+	// очищаем память выделенную для элементов строк
+	for (int i = 0; i < rowsCount; i++)
+		delete[] data[i];
+	// очищаем память выделенную под строки
+	delete[] data;
+}
+
+int Matrix::RowsCount()
+{
+	return rowsCount;
+}
+
+int Matrix::ColumnsCount()
+{
+	return columnsCount;
+}
+
+int Matrix::GetValue(int column, int row)
+{
+	return data[row][column];
+}
+
+bool Matrix::SetValue(int column, int row, int value)
+{
+	data[row][column] = value;
+	return true;
+}
+
+std::string Matrix::ToString()
+{
+	std::string str = "";
+	for (int i = 0; i < rowsCount; i++)
+	{
+		for (int j = 0; j < columnsCount; j++)
+		{
+			str += std::to_string(data[i][j]) + " ";
+		}
+		str += "\n";
+	}
+	str += "\0";
+	return str;
+}
+
+void Matrix::SortRow(int row)
+{
+	// Сортируем пузырьком data[row]
+	int temp = 0;
+	for (int i = 0; i < columnsCount - 1; i++) 
+		for (int j = 0; j < columnsCount - i - 1; j++)
+			if (data[row][j] > data[row][j + 1]) 
+			{
+				temp = data[row][j];
+				data[row][j] = data[row][j + 1];
+				data[row][j + 1] = temp;
+			}
+}
+
+void Matrix::SortColumn(int column)
+{
+	int temp = 0;
+	// Сортируем пузырьком data[][column]
+	for (int i = 0; i < rowsCount - 1; i++)
+		for (int j = 0; j < rowsCount - i - 1; j++)
+			if (data[j][column] > data[j + 1][column])
+			{
+				temp = data[j][column];
+				data[j][column] = data[j + 1][column];
+				data[j + 1][column] = temp;
+			}
+}
+
+int Matrix::Id()
+{
+	return id;
+}
