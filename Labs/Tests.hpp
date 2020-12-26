fef4e6796f02c9bb2cf91302652a7b161117cc93
Darya <d.gorshkova8@gmail.com>
2020-12-26 17:50:52 +0700

    operators tests

--- Labs/Tests.hpp
+++ Labs/Tests.hpp
@@ -106,6 +106,96 @@ namespace MatrixTests
 		std::cout << "\t(sort column 0 in ([0], [-5], [4]) and check matrix[0][2] == 4): ";
 		AssertEqual(matrix.GetValue(0, 2), 4);
 	}
+	
+	void MatrixAssignmentTest()
+	{
+		std::cout << "MatrixAssignmentTest:\n";
+		Matrix m1;
+		Matrix m2(2,2);
+		m2.SetValue(0, 0, 1);
+		m2.SetValue(1, 0, 2);
+		m2.SetValue(0, 1, 3);
+		m2.SetValue(1, 1, 4);
+		m1 = m2;
+		std::cout << "\t(deafult assignment ([1,2],[3,4]) and check rows count = 2): ";
+		AssertEqual(m1.RowsCount(), 2);
+		std::cout << "\t(deafult assignment ([1,2],[3,4]) and check columns count = 2): ";
+		AssertEqual(m1.ColumnsCount(), 2);
+		std::cout << "\t(deafult assignment ([1,2],[3,4]) and check (0,0) = 1): ";
+		AssertEqual(m1.GetValue(0, 0), 1);
+		std::cout << "\t(deafult assignment ([1,2],[3,4]) and check (1,0) = 2): ";
+		AssertEqual(m1.GetValue(1, 0), 2);
+		std::cout << "\t(deafult assignment ([1,2],[3,4]) and check (0,1) = 3): ";
+		AssertEqual(m1.GetValue(0, 1), 3);
+		std::cout << "\t(deafult assignment ([1,2],[3,4]) and check (1,1) = 4): ";
+		AssertEqual(m1.GetValue(1, 1), 4);
+	}
+
+	void MatrixSummTest()
+	{
+		std::cout << "MatrixSummTest:\n";
+		Matrix m1(2, 2);
+		m1.SetValue(0, 0, 1);
+		m1.SetValue(1, 0, 1);
+		m1.SetValue(0, 1, -1);
+		m1.SetValue(1, 1, 0);
+		Matrix m2(2, 2);
+		m2.SetValue(0, 0, 1);
+		m2.SetValue(1, 0, 2);
+		m2.SetValue(0, 1, 3);
+		m2.SetValue(1, 1, 4);
+		Matrix m3 = m2 + m1;
+		std::cout << "\t( ([1,2],[3,4]) + ([1,1],[-1,0]) and check (0,0) = 2): ";
+		AssertEqual(m3.GetValue(0, 0), 2);
+		std::cout << "\t( ([1,2],[3,4]) + ([1,1],[-1,0]) and check (1,0) = 3): ";
+		AssertEqual(m3.GetValue(1, 0), 3);
+		std::cout << "\t( ([1,2],[3,4]) + ([1,1],[-1,0]) and check (0,1) = 2): ";
+		AssertEqual(m3.GetValue(0, 1), 2);
+		std::cout << "\t( ([1,2],[3,4]) + ([1,1],[-1,0]) and check (1,1) = 4): ";
+		AssertEqual(m3.GetValue(1, 1), 4);
+	}
+
+	void MatrixMinusTest()
+	{
+		std::cout << "MatrixMinusTest:\n";
+		Matrix m1(2, 2);
+		m1.SetValue(0, 0, 1);
+		m1.SetValue(1, 0, 1);
+		m1.SetValue(0, 1, -1);
+		m1.SetValue(1, 1, 0);
+		Matrix m2(2, 2);
+		m2.SetValue(0, 0, 1);
+		m2.SetValue(1, 0, 2);
+		m2.SetValue(0, 1, 3);
+		m2.SetValue(1, 1, 4);
+		Matrix m3 = m2 - m1;
+		std::cout << "\t( ([1,2],[3,4]) - ([1,1],[-1,0]) and check (0,0) = 0): ";
+		AssertEqual(m3.GetValue(0, 0), 0);
+		std::cout << "\t( ([1,2],[3,4]) - ([1,1],[-1,0]) and check (1,0) = 1): ";
+		AssertEqual(m3.GetValue(1, 0), 1);
+		std::cout << "\t( ([1,2],[3,4]) - ([1,1],[-1,0]) and check (0,1) = 4): ";
+		AssertEqual(m3.GetValue(0, 1), 4);
+		std::cout << "\t( ([1,2],[3,4]) - ([1,1],[-1,0]) and check (1,1) = 4): ";
+		AssertEqual(m3.GetValue(1, 1), 4);
+	}
+
+	void MatrixIndexOperatorTest()
+	{
+		std::cout << "MatrixIndexOperatorTest:\n";
+		Matrix m2(2, 2);
+		m2.SetValue(0, 0, 1);
+		m2.SetValue(1, 0, 2);
+		m2.SetValue(0, 1, 3);
+		m2.SetValue(1, 1, 4);
+		std::cout << "\t( ([1,2],[3,4]) index 0 = 1): ";
+		AssertEqual(m2[0], 1);
+		std::cout << "\t( ([1,2],[3,4]) index 1 = 2): ";
+		AssertEqual(m2[1], 2);
+		std::cout << "\t( ([1,2],[3,4]) index 2 = 3): ";
+		AssertEqual(m2[2], 3);
+		std::cout << "\t( ([1,2],[3,4]) index 3 = 4): ";
+		AssertEqual(m2[3], 4);
+	}
 
 	void FullTest()
 	{
@@ -118,6 +208,14 @@ namespace MatrixTests
 		MatrixSortRowTest();
 		std::cout << std::endl;
 		MatrixSortColumnTest();
+		std::cout << std::endl;
+		MatrixAssignmentTest();
+		std::cout << std::endl;
+		MatrixSummTest();
+		std::cout << std::endl;
+		MatrixMinusTest();
+		std::cout << std::endl;
+		MatrixIndexOperatorTest();
 		std::cout << std::endl;
 	}
 }
